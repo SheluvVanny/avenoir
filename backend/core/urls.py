57df96564b18c34a8820256558137df51d6abcfb
yvan <ivanemanj@gmail.com>
2025-05-07 19:41:21 +0100

    add GET /api/events/<id>/ endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import event_list_create
+from .views import event_list_create, event_detail
 
 urlpatterns = [
     path('events/', event_list_create, name='event_list_create'),
+    path('events/<int:pk>/', event_detail, name='event_detail'),
 ]
 
--- backend/core/views.py
+++ backend/core/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Event
 from .serializers import EventSerializer
+from django.shortcuts import get_object_or_404
+
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
@@ -19,3 +21,9 @@ def event_list_create(request):
             event = serializer.save(created_by=request.user)
             return Response(EventSerializer(event).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    serializer = EventSerializer(event)
+    return Response(serializer.data)
